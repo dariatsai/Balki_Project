4ce79aa53325b3c8584073ea238a6b5733c061e1
dariatsai <d9215650086@gmail.com>
2025-04-28 21:40:01 +0300

    abcв

--- src/Grath.java
+++ src/Grath.java
@@ -16,7 +16,7 @@ public class Grath {
                 final int SCALE = 10; // шкала пискселей, размер клетки
                 final int WIDTH = 101; // ширина
                 int HEIGHT = 101;// высота
-                double length1 = (double) Math.round(length/WIDTH*100)/10;
+                double length1 = (double) Math.round(length/(WIDTH-1)*100)/10;
                 g.setColor(Color.GRAY);
                 for (int x = 0; x <= WIDTH * SCALE; x += SCALE) {
                     g.drawLine(x, 0, x, HEIGHT * SCALE);
@@ -31,7 +31,7 @@ public class Grath {
                     double nagruzka=weight*ra*rb*length;
                     double maxM=ra*coordinate;
 
-                    int coordinate1 = (int) Math.round(coordinate*WIDTH/length*100)/100;
+                    int coordinate1 = (int) Math.round(coordinate*(WIDTH-1)/length*100)/100;
                     int scale = 1;  // количество знаков после запятой
                     BigDecimal maxp = new BigDecimal(maxM);
                     maxp = maxp.setScale(scale, RoundingMode.HALF_UP);
@@ -59,18 +59,26 @@ public class Grath {
 
                     double b=length-coordinate;
                     double A=weight*b*b/length/length/length*(3*coordinate+b);
-                    double MA=(-1)*weight*coordinate*b*b/length/length;
-                    double MB=(-1)*weight*coordinate*coordinate*b/length/length;
-                    double Mmax=A*coordinate+MA;
+                    double MA=weight*coordinate*b*b/length/length;
+                    double MB=weight*coordinate*coordinate*b/length/length;
+                    double Mmax=(-1)*A*coordinate+MA;
+
+                    g.setColor(Color.darkGray);
+                    g.fillRect(10, (HEIGHT-1) / 2 * 10-10, WIDTH*10-10, 20);
+
+                    g.setColor(Color.RED);
+                    g.drawLine(0, (HEIGHT-1) / 2 * 10, WIDTH * 10, (HEIGHT-1) / 2 * 10);
+                    g.drawLine(10, 0, 10, HEIGHT * 10);
+                    for(int i=0;i<=WIDTH/10;i++) {
+                        g.drawString("" + i*length1, 100 * i + 10, HEIGHT / 2 * 10+10);
+                    }
 
                     if(MA>MB) {
-                        if (Mmax > MB*(-1)) {
-                            double delenie = (double) Math.round(Mmax/10*100) / 100;
-                            int coordinate1 = (int) Math.round(coordinate * WIDTH / length * 100) / 100;
-                            int coordinateMa=(int) Math.round(coordinate1 * MA/Mmax*100) / 100;
-                            int coordinateMb=(int) Math.round(coordinate1 * MB/Mmax*100) / 100;
-                            g.setColor(Color.darkGray);
-                            g.fillRect(10, (HEIGHT - 1) / 2 * 10 + 20, 1000, 20);
+                        if (Mmax*(-1) > MB) {
+                            double delenie = (double) Math.round(Mmax/5*100) / 100;
+                            int coordinate1 = (int) Math.round(coordinate * (WIDTH-1) / length * 1000) / 100+10;
+                            int coordinateMa= 10;
+                            int coordinateMb=WIDTH*10;
 
                             g.setColor(Color.RED);
                             g.drawLine(0, (HEIGHT - 1) / 2 * 10, WIDTH * 10, (HEIGHT - 1) / 2 * 10);
@@ -80,13 +88,6 @@ public class Grath {
 
                         }
                     }
-
-                    g.setColor(Color.darkGray);
-                    g.fillRect(10, (HEIGHT-1) / 2 * 10 +20, 1000, 20);
-
-                    g.setColor(Color.RED);
-                    g.drawLine(0, (HEIGHT-1) / 2 * 10, WIDTH * 10, (HEIGHT-1) / 2 * 10);
-                    g.drawLine(10, 0, 10, HEIGHT * 10);
                 }
 
             }
--- src/MyFrame.java
+++ src/MyFrame.java
@@ -13,6 +13,7 @@ public class MyFrame extends JFrame implements KeyEventDispatcher {
     TextField textField;
     JTextField LengthField;
     JTextField WeightField;
+
     JTextField CoordinateField;
     List<JTextField> textFieldList = new ArrayList<>();
     String s = "";
@@ -21,20 +22,50 @@ public class MyFrame extends JFrame implements KeyEventDispatcher {
         JTabbedPane tabsLeft = new JTabbedPane(JTabbedPane.LEFT, JTabbedPane.SCROLL_TAB_LAYOUT);
         for (int i = 1; i <= 3; i++) {
             JPanel panel = new JPanel();
-            //GridLayout layout = new GridLayout(2, 0, 5, 12);
-            //panel.setLayout(layout);
+
 
 
             if (i == 1) {
-                JButton button1 = new JButton("Button");
-                //button1.setBounds(1, 800, 200, 100);
-                panel.add(BorderLayout.WEST,button1);
-                //JPanel p = new JPanel(new SpringLayout());
-                //panel.add(p);                // Добавление вкладки
+                JButton button1 = new JButton("Выбрать");
+                panel.add(BorderLayout.WEST,button1);// Добавление вкладки
+                String[] namber = {
+                        "10",
+                        "12",
+                        "14",
+                        "16",
+                        "18",
+                        "18a",
+                        "20",
+                        "20a",
+                        "22",
+                        "22a",
+                        "24",
+                        "24a",
+                        "27",
+                        "27a",
+                        "30",
+                        "30a",
+                        "33",
+                        "33",
+                        "36",
+                        "40",
+                        "45",
+                        "50",
+                        "55",
+                        "60"
+                };
+                JComboBox editComboBox = new JComboBox(namber);
+                editComboBox.setEditable(true);
+                panel.add(editComboBox);
+                ImageIcon imageIcon1 = new ImageIcon("двутаврпараметры.jpg");
+                JLabel label1 = new JLabel(imageIcon1);
+                panel.add(label1);
+                ImageIcon imageIcon2 = new ImageIcon("двутаврномер.jpg");
+                JLabel label2 = new JLabel(imageIcon2);
+                panel.add(label2);
                 tabsLeft.addTab("Сечения", panel);                // Подключение мнемоники
                 tabsLeft.setMnemonicAt(i - 1, String.valueOf(i).charAt(0));
             }
-
 
             if (i == 2) {
 
@@ -51,19 +82,19 @@ public class MyFrame extends JFrame implements KeyEventDispatcher {
             if (i==3){
 
 
-                JLabel lengthLabel = new JLabel("Длина: ");
+                JLabel lengthLabel = new JLabel("Длина(м): ");
                 lengthLabel.setLabelFor(LengthField);
                 JTextField lenthfield = new JTextField(10);
                 panel.add(lengthLabel);
                 panel.add(lenthfield);
 
-                JLabel weightLabel = new JLabel("Вес груза: ");
+                JLabel weightLabel = new JLabel("Вес груза(т): ");
                 weightLabel.setLabelFor(WeightField);
                 JTextField weightfield = new JTextField(10);
                 panel.add(weightLabel);
                 panel.add(weightfield);
 
-                JLabel coordinateLabel = new JLabel("Координата груза: ");
+                JLabel coordinateLabel = new JLabel("Координата груза(м): ");
                 coordinateLabel.setLabelFor(WeightField);
                 JTextField coordinatefield = new JTextField(10);
                 panel.add(coordinateLabel);
